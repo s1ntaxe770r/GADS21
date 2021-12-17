9cdd956ae65706c2e02404d5e90cb27cd8760d8d
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-12-17 17:16:08 +0100

    scaffold

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.3.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region = var.region
+  credentials = file()
+}
+
