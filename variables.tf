2fc70de9de64d40d076b48f23b20eb6e72880e35
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-12-20 13:29:02 +0100

    adds more variables

--- variables.tf
+++ variables.tf
@@ -9,7 +9,24 @@ variable "region" {
 }
 
 
-variable "creds_path"{
-description = "google cloud service account path"
-default = "./gads21-service.json"
+variable "creds_path" {
+  description = "google cloud service account path"
+  default     = "./gads21-service.json"
 }
+
+variable "node_count" {
+  description = "kubernetes node count (defaults to 1)"
+  default     = 1
+}
+
+variable "machine_type" {
+  description = "gke machine to be used for a node(defaults to e2.medium)"
+  default     = "e2.medium"
+}
+
+variable "sa_id" {
+  description = "gcp service account id"
+  default     = "projects/gads21/serviceAccounts/gads21@gads21.iam.gserviceaccount.com"
+}
+
+
