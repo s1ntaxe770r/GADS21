05f683ecb9a9276558e33d6a571527cc22ad7686
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-12-20 13:29:27 +0100

    add service account as data source

--- main.tf
+++ main.tf
@@ -8,8 +8,37 @@ terraform {
 }
 
 provider "google" {
-  project = var.project
-  region = var.region
-  credentials = file()
+  project     = var.project
+  region      = var.region
+  credentials = file(var.creds_path)
+}
+
+data "google_service_account" "gke_sa" {
+  account_id = var.sa_id
+}
+
+
+resource "google_container_cluster" "primary" {
+  name                     = "gads21-${terraform.workspace}"
+  location                 = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_node_pool" {
+  name       = "gads21-${terraform.workspace}-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.node_count
+
+  node_config {
+    preemptible     = true
+    machine_type    = var.machine_type
+    service_account = data.google_service_account.gke_sa.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
 }
 
